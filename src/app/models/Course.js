0468e64a6a347f4983882fe549100f6b8ce045e1
DangPham <dangpham112000@users.noreply.github.com>
2021-07-16 21:56:36 +0700

    Auto increment id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
 const mongooseDelete = require('mongoose-delete');
-
-// Add plugin
-mongoose.plugin(slug);
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
-const CourseSchema = new Schema({
-    name: { type: String, required: true },
-    description: { type: String },
-    image: { type: String },
-    videoId: { type: String, required: true },
-    level: { type: String },
-    slug: { type: String, slug: 'name', unique: true },
-}, {
-    timestamps: true,
-});
+const CourseSchema = new Schema(
+    {
+        _id: { type: Number, },
+        name: { type: String, required: true },
+        description: { type: String },
+        image: { type: String },
+        videoId: { type: String, required: true },
+        level: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+    }, 
+    {
+        _id: false,
+        timestamps: true,
+    },
+);
 
 // Custom query helpers
 CourseSchema.query.sortable = function(req) {
@@ -30,6 +33,9 @@ CourseSchema.query.sortable = function(req) {
 }
 
 // Add plugin
+mongoose.plugin(slug);
+
+CourseSchema.plugin(AutoIncrement);
 CourseSchema.plugin(mongooseDelete, { 
     overrideMethods: 'all', 
     deletedAt : true, 
