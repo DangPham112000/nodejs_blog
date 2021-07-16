9e21e7d8476796d04c877e92379a82be9807e1c8
DangPham <dangpham112000@users.noreply.github.com>
2021-07-16 17:51:42 +0700

    sort middleware

---
+++ src/app/controller/MeController.js
@@ -0,0 +1,38 @@
+// const { text } = require("express");
+const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
+
+class MeController {
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+
+        let courseQuery = Course.find({});
+
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
+        
+        
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
+            .then(([courses, deleteCount]) => {
+                res.render('me/stored-courses', {
+                    deleteCount,
+                    courses: multipleMongooseToObject(courses),
+                })
+            })
+            .catch(next);
+    }
+
+    // [GET] /me/trash/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then(courses => res.render('me/trash-courses', {
+                courses: multipleMongooseToObject(courses)
+            }))
+            .catch(next);
+    }
+}
+
+module.exports = new MeController();
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const slug = require('mongoose-slug-generator');
+const mongooseDelete = require('mongoose-delete');
 
+// Add plugin
 mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
@@ -14,6 +16,12 @@ const Course = new Schema({
     slug: { type: String, slug: 'name', unique: true },
 }, {
     timestamps: true,
+});
+
+// Add plugin
+Course.plugin(mongooseDelete, { 
+    overrideMethods: 'all', 
+    deletedAt : true, 
 });
 
 module.exports = mongoose.model('Course', Course);
